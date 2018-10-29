c70e6432104993374405a7850faeb9d1df904d01
Martin Bauer <martin.bauer@fau.de>
2018-10-29 15:58:48 +0100

    Increased test coverage

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
-from .walberla_lbm_generation import generate_lattice_model_files, generate_lattice_model, RefinementScaling, create_lb_method
+from .walberla_lbm_generation import generate_lattice_model_files, generate_lattice_model, RefinementScaling, \
+    create_lb_method
 from pystencils import Field
+
+__all__ = ['Field', 'generate_lattice_model_files', 'generate_lattice_model', 'RefinementScaling', 'create_lb_method']
